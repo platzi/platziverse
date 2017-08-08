920bab50c769154556bbebb19970392f8923b38d
Julian Duque <julianduquej@gmail.com>
2017-08-08 11:41:12 -0500

    [db] implement basic setup database function
    - Setup relations between Agent and Metric

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,6 +1,19 @@
 'use strict'
 
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+
 module.exports = async function (config) {
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
+  await sequelize.authenticate()
+
   const Agent = {}
   const Metric = {}
 

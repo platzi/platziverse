ffef08ca40dd2dc8589baf5c1d28b4428ba79cb9
Julian Duque <julianduquej@gmail.com>
2017-08-08 18:48:29 -0500

    [db] add example

---
+++ platziverse-db/examples/index.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const db = require('../')
+
+async function run () {
+  const config = {
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USER || 'platzi',
+    password: process.env.DB_PASS || 'platzi',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres'
+  }
+
+  const { Agent, Metric } = await db(config).catch(handleFatalError)
+
+  const agent = await Agent.createOrUpdate({
+    uuid: 'yyy',
+    name: 'test',
+    username: 'test',
+    hostname: 'test',
+    pid: 1,
+    connected: true
+  }).catch(handleFatalError)
+
+  console.log('--agent--')
+  console.log(agent)
+
+  const agents = await Agent.findAll().catch(handleFatalError)
+  console.log('--agents--')
+  console.log(agents)
+
+  const metrics = await Metric.findByAgentUuid(agent.uuid).catch(handleFatalError)
+  console.log('--metrics--')
+  console.log(metrics)
+
+  const metric = await Metric.create(agent.uuid, {
+    type: 'memory',
+    value: '300'
+  }).catch(handleFatalError)
+
+  console.log('--metric--')
+  console.log(metric)
+
+  const metricsByType = await Metric.findByTypeAgentUuid('memory', agent.uuid).catch(handleFatalError)
+  console.log('--metrics--')
+  console.log(metricsByType)
+}
+
+function handleFatalError (err) {
+  console.error(err.message)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+run()

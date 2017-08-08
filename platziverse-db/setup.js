c228cc93b4a22625620b13648a4f3425f598552b
Julian Duque <julianduquej@gmail.com>
2017-08-08 12:45:29 -0500

    [db] add confirmation prompt and colorize errors
    - Use inquirer for prompts and chalk for colors

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,9 +1,25 @@
 'use strict'
 
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./')
 
+const prompt = inquirer.createPromptModule()
+
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
@@ -21,7 +37,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }

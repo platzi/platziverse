9919196f2c7fb663cc5f8cfa39bb94679fbc481a
Julian Duque <julianduquej@gmail.com>
2017-08-09 17:23:54 -0500

    [api] implement express error handling

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -10,8 +10,13 @@ api.get('/agents', (req, res) => {
   res.send({})
 })
 
-api.get('/agent/:uuid', (req, res) => {
+api.get('/agent/:uuid', (req, res, next) => {
   const { uuid } = req.params
+
+  if (uuid !== 'yyy') {
+    return next(new Error('Agent not found'))
+  }
+
   res.send({ uuid })
 })
 
--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const debug = require('debug')('platziverse:api')
 const http = require('http')
 const chalk = require('chalk')
 const express = require('express')
@@ -11,6 +12,26 @@ const app = express()
 const server = http.createServer(app)
 
 app.use('/api', api)
+
+// Express Error Handler
+app.use((err, req, res, next) => {
+  debug(`Error: ${err.message}`)
+
+  if (err.message.match(/not found/)) {
+    return res.status(404).send({ error: err.message })
+  }
+
+  res.status(500).send({ error: err.message })
+})
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
 
 server.listen(port, () => {
   console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)

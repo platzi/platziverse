a37ce0b48147e40e3245d3b8e4c73bd90c87e0dc
Julian Duque <julianduquej@gmail.com>
2017-08-09 11:38:07 -0500

    [mqtt] implement mqtt events
    - clientConnected
    - clientDisconnected
    - published
    - error handling

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -18,6 +18,30 @@ const settings = {
 
 const server = new mosca.Server(settings)
 
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+  debug(`Client Disconnected: ${client.id}`)
+})
+
+server.on('published', (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+  debug(`Payload: ${packet.payload}`)
+})
+
 server.on('ready', () => {
   console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
 })
+
+server.on('error', handleFatalError)
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)

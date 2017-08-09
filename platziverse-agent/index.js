fd85e7a4de4b0209f8555bbef093e5d611bdc3cf
Julian Duque <julianduquej@gmail.com>
2017-08-09 12:57:49 -0500

    [agent] implement basic EventEmitter

---
+++ platziverse-agent/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const EventEmitter = require('events')
+
+class PlatziverseAgent extends EventEmitter {
+  constructor () {
+    super()
+  }
+
+}
+
+module.exports = PlatziverseAgent

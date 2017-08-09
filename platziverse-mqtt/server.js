59b63f181bc2103b1a3d32adbe14c5b5351cdb31
Julian Duque <julianduquej@gmail.com>
2017-08-09 12:06:25 -0500

    [mqtt] integrate platziverse-db

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -4,6 +4,7 @@ const debug = require('debug')('platziverse:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
+const db = require('platziverse-db')
 
 const backend = {
   type: 'redis',
@@ -16,7 +17,18 @@ const settings = {
   backend
 }
 
+const config = {
+  database: process.env.DB_NAME || 'platziverse',
+  username: process.env.DB_USER || 'platzi',
+  password: process.env.DB_PASS || 'platzi',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  logging: s => debug(s)
+}
+
 const server = new mosca.Server(settings)
+
+let Agent, Metric
 
 server.on('clientConnected', client => {
   debug(`Client Connected: ${client.id}`)
@@ -31,7 +43,12 @@ server.on('published', (packet, client) => {
   debug(`Payload: ${packet.payload}`)
 })
 
-server.on('ready', () => {
+server.on('ready', async () => {
+  const services = await db(config).catch(handleFatalError)
+
+  Agent = services.Agent
+  Metric = services.Metric
+
   console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
 })
 

8e1b4d3992fdff781f48d409395a49d77a3b6d4d
Julian Duque <julianduquej@gmail.com>
2017-08-09 11:21:52 -0500

    [mqtt] implement basic mqtt server structure

---
+++ platziverse-mqtt/server.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const debug = require('debug')('platziverse:mqtt')
+const mosca = require('mosca')
+const redis = require('redis')
+const chalk = require('chalk')
+
+const backend = {
+  type: 'redis',
+  redis,
+  return_buffers: true
+}
+
+const settings = {
+  port: 1883,
+  backend
+}
+
+const server = new mosca.Server(settings)
+
+server.on('ready', () => {
+  console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
+})

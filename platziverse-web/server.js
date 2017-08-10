2b1cd3ac977c1f0586c5b0872bd9d3240e35f932
Julian Duque <julianduquej@gmail.com>
2017-08-10 11:10:18 -0500

    [web] implement basic socket.io

--- platziverse-web/server.js
+++ platziverse-web/server.js
@@ -4,13 +4,28 @@ const debug = require('debug')('platziverse:web')
 const http = require('http')
 const path = require('path')
 const express = require('express')
+const socketio = require('socket.io')
 const chalk = require('chalk')
 
 const port = process.env.PORT || 8080
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Socket.io / WebSockets
+io.on('connect', socket => {
+  debug(`Connected ${socket.id}`)
+
+  socket.on('agent/message', payload => {
+    console.log(payload)
+  })
+
+  setInterval(() => {
+    socket.emit('agent/message', { agent: 'xxx-yyy' })
+  }, 2000)
+})
 
 function handleFatalError (err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)

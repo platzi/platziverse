58f2b2322ba02104596df3d8d7541c3b63ca0f8b
Julian Duque <julianduquej@gmail.com>
2017-08-10 18:18:22 -0500

    [cli] implement graphics

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -14,6 +14,11 @@ const screen = blessed.screen()
 
 const agents = new Map()
 const agentMetrics = new Map()
+let extended = []
+let selected = {
+  uuid: null,
+  type: null
+}
 
 const grid = new contrib.grid({
   rows: 1,
@@ -86,14 +91,31 @@ agent.on('agent/message', payload => {
   renderData()
 })
 
+tree.on('select', node => {
+  const { uuid } = node
+
+  if (node.agent) {
+    node.extended ? extended.push(uuid) : extended = extended.filter(e => e !== uuid)
+    selected.uuid = null
+    selected.type = null
+    return
+  }
+
+  selected.uuid = uuid
+  selected.type = node.type
+
+  renderMetric()
+})
+
 function renderData () {
   const treeData = {}
-
+  let idx = 0
   for (let [ uuid, val ] of agents) {
     const title = ` ${val.name} - (${val.pid})`
     treeData[title] = {
       uuid,
       agent: true,
+      extended: extended.includes(uuid),
       children: {}
     }
 
@@ -105,7 +127,7 @@ function renderData () {
         metric: true
       }
 
-      const metricName = ` ${type}`
+      const metricName = ` ${type} ${' '.repeat(1000)} ${idx++}`
       treeData[title].children[metricName] = metric
     })
   }
@@ -114,6 +136,26 @@ function renderData () {
     extended: true,
     children: treeData
   })
+
+  renderMetric()
+}
+
+function renderMetric () {
+  if (!selected.uuid && !selected.type) {
+    line.setData([{ x: [], y: [], title: '' }])
+    screen.render()
+    return
+  }
+
+  const metrics = agentMetrics.get(selected.uuid)
+  const values = metrics[selected.type]
+  const series = [{
+    title: selected.type,
+    x: values.map(v => v.timestamp).slice(-10),
+    y: values.map(v => v.value).slice(-10)
+  }]
+
+  line.setData(series)
   screen.render()
 }
 

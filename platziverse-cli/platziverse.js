731e25fe68c0d856853db5eb999ad874c31c6e48
Julian Duque <julianduquej@gmail.com>
2017-08-10 17:20:33 -0500

    [cli] create basic layout with blessed

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -2,4 +2,32 @@
 
 'use strict'
 
-console.log('Hello Platziverse!')
+/* eslint new-cap: "off" */
+
+const blessed = require('blessed')
+const contrib = require('blessed-contrib')
+
+const screen = blessed.screen()
+
+const grid = new contrib.grid({
+  rows: 1,
+  cols: 4,
+  screen
+})
+
+const tree = grid.set(0, 0, 1, 1, contrib.tree, {
+  label: 'Connected Agents'
+})
+
+const line = grid.set(0, 1, 1, 3, contrib.line, {
+  label: 'Metric',
+  showLegend: true,
+  minY: 0,
+  xPadding: 5
+})
+
+screen.key([ 'escape', 'q', 'C-c' ], (ch, key) => {
+  process.exit(0)
+})
+
+screen.render()

0f4eac1ad04ebe220b1b88ed9a659f473593c840
Julian Duque <julianduquej@gmail.com>
2017-08-10 10:55:33 -0500

    [web] basic static server with express

---
+++ platziverse-web/server.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const debug = require('debug')('platziverse:web')
+const http = require('http')
+const path = require('path')
+const express = require('express')
+const chalk = require('chalk')
+
+const port = process.env.PORT || 8080
+const app = express()
+const server = http.createServer(app)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
+
+server.listen(port, () => {
+  console.log(`${chalk.green('[platziverse-web]')} server listening on port ${port}`)
+})

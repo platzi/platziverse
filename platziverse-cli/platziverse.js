564b2a46aef40365856bf51b4376c30f1370a3eb
Julian Duque <julianduquej@gmail.com>
2017-08-10 17:32:27 -0500

    [cli] implement agent/connected

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -6,8 +6,13 @@
 
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
+const PlatziverseAgent = require('platziverse-agent')
 
+const agent = new PlatziverseAgent()
 const screen = blessed.screen()
+
+const agents = new Map()
+const agentMetrics = new Map()
 
 const grid = new contrib.grid({
   rows: 1,
@@ -26,8 +31,39 @@ const line = grid.set(0, 1, 1, 3, contrib.line, {
   xPadding: 5
 })
 
+agent.on('agent/connected', payload => {
+  const { uuid } = payload.agent
+
+  if (!agents.has(uuid)) {
+    agents.set(uuid, payload.agent)
+    agentMetrics.set(uuid, {})
+  }
+
+  renderData()
+})
+
+function renderData () {
+  const treeData = {}
+
+  for (let [ uuid, val ] of agents) {
+    const title = ` ${val.name} - (${val.pid})`
+    treeData[title] = {
+      uuid,
+      agent: true,
+      children: {}
+    }
+  }
+
+  tree.setData({
+    extended: true,
+    children: treeData
+  })
+  screen.render()
+}
+
 screen.key([ 'escape', 'q', 'C-c' ], (ch, key) => {
   process.exit(0)
 })
 
+agent.connect()
 screen.render()

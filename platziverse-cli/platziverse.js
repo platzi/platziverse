cf3e971ab029d2af9ff59659af063723f6a8de4c
Julian Duque <julianduquej@gmail.com>
2017-08-10 17:46:57 -0500

    [cli] implement agent/message

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -6,6 +6,7 @@
 
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
+const moment = require('moment')
 const PlatziverseAgent = require('platziverse-agent')
 
 const agent = new PlatziverseAgent()
@@ -42,6 +43,49 @@ agent.on('agent/connected', payload => {
   renderData()
 })
 
+agent.on('agent/disconnected', payload => {
+  const { uuid } = payload.agent
+
+  if (agents.has(uuid)) {
+    agents.delete(uuid)
+    agentMetrics.delete(uuid)
+  }
+
+  renderData()
+})
+
+agent.on('agent/message', payload => {
+  const { uuid } = payload.agent
+  const { timestamp } = payload
+
+  if (!agents.has(uuid)) {
+    agents.set(uuid, payload.agent)
+    agentMetrics.set(uuid, {})
+  }
+
+  const metrics = agentMetrics.get(uuid)
+
+  payload.metrics.forEach(m => {
+    const { type, value } = m
+
+    if (!Array.isArray(metrics[type])) {
+      metrics[type] = []
+    }
+
+    const length = metrics[type].length
+    if (length >= 20) {
+      metrics[type].shift()
+    }
+
+    metrics[type].push({
+      value,
+      timestamp: moment(timestamp).format('HH:mm:ss')
+    })
+  })
+
+  renderData()
+})
+
 function renderData () {
   const treeData = {}
 
@@ -52,6 +96,18 @@ function renderData () {
       agent: true,
       children: {}
     }
+
+    const metrics = agentMetrics.get(uuid)
+    Object.keys(metrics).forEach(type => {
+      const metric = {
+        uuid,
+        type,
+        metric: true
+      }
+
+      const metricName = ` ${type}`
+      treeData[title].children[metricName] = metric
+    })
   }
 
   tree.setData({
@@ -66,4 +122,5 @@ screen.key([ 'escape', 'q', 'C-c' ], (ch, key) => {
 })
 
 agent.connect()
+tree.focus()
 screen.render()

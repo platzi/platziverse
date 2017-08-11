7ca94e2ac40f8cf8190c0f4874fd25373bd9fe6b
Julian Duque <julianduquej@gmail.com>
2017-08-11 10:58:21 -0500

    [db] and --yes flag to setup script

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -3,21 +3,25 @@
 const debug = require('debug')('platziverse:db:setup')
 const inquirer = require('inquirer')
 const chalk = require('chalk')
+const minimist = require('minimist')
 const db = require('./')
 
+const args = minimist(process.argv)
 const prompt = inquirer.createPromptModule()
 
 async function setup () {
-  const answer = await prompt([
-    {
-      type: 'confirm',
-      name: 'setup',
-      message: 'This will destroy your database, are you sure?'
-    }
-  ])
+  if (!args.yes) {
+    const answer = await prompt([
+      {
+        type: 'confirm',
+        name: 'setup',
+        message: 'This will destroy your database, are you sure?'
+      }
+    ])
 
-  if (!answer.setup) {
-    return console.log('Nothing happened :)')
+    if (!answer.setup) {
+      return console.log('Nothing happened :)')
+    }
   }
 
   const config = {
